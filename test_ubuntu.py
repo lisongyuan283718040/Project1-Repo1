e97d0b94995db63c12cd5a1e0a993f91b7f72001
lisongyuan283718040 <283718040@qq.com>
2023-06-29 15:27:56 +0800

    add test_ubuntu.py

---
+++ test_ubuntu.py
@@ -0,0 +1,15 @@
+class TestClass3:
+
+    def fun31(self):
+        print("Ubuntu fun31")
+
+    def fun32(self):
+        print("Ubuntu fun32")
+
+    def fun33(self):
+        print("Ubuntu fun33")
+
+if __name__ == '__main__':
+    TestClass3.fun31(self="fun31")
+    TestClass3.fun32(self="fun32")
+    TestClass3.fun33(self="fun33")
