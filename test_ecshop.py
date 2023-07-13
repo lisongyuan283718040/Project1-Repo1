00b1bb070940107ec916e914e80b7d0914d942fb
lisongyuan283718040 <283718040@qq.com>
2023-07-13 14:13:14 +0800

    add 1)email_manage.py 2)test_ecshop.py 3)test_start.py

--- test_ecshop.py
+++ test_ecshop.py
@@ -22,16 +22,15 @@ class TestEcshop(unittest.TestCase):
         print('test case3')
         assert 2 == 3
 
-if __name__ == '__main__':
-    current_path = os.getcwd()
-    # print("current_path is: " + current_path) # for debug
-    suite = unittest.defaultTestLoader.discover(current_path, 'test_ecshop.py')
-    abs_file = current_path + '\\' + r'report.html'
-    print("abs_file is: " + abs_file) # for debug
-    files = open(abs_file, 'wb')
-    runner = HTMLTestRunner(stream=files, title='CodeSend2Mails', description='AllTestCasesResults')
-    runner.run(suite)
-    files.close() # 在发动之前一定要把文件流关闭
-    # 发送邮件
-    time.sleep(3) # should give some minutes to wait generating report then can send successfully
-    EmailManage().send_email(files.name)
+# if __name__ == '__main__':
+#     current_path = os.getcwd()
+#     suite = unittest.defaultTestLoader.discover(current_path, 'test_ecshop.py')
+#     abs_file = current_path + '\\' + r'report.html'
+#     print("abs_file is: " + abs_file) # for debug
+#     files = open(abs_file, 'wb')
+#     runner = HTMLTestRunner(stream=files, title='test_ecshop.py report', description='test_ecshop.py results')
+#     runner.run(suite)
+#     files.close() # 在发送邮件之前一定要把文件流关闭
+#     # 发送邮件
+#     time.sleep(3) # should give some minutes to wait generating report then can send successfully
+#     EmailManage().send_email(files.name)
